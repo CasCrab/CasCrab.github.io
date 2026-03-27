cab58d955086fbefc79675183b3e499cd479b4d7
CasCrab <kopfstein.blitz_7y@icloud.com>
2026-03-27 13:29:19 +0000

    Deploying to gh-pages from @ CasCrab/CasCrab.github.io@7ee0b52bff7d63a7d73897dc488a6fb982037336 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-vnegnn-bindingsite-10ev",
+        },{id: "post-vnegnn-bindingsite-10ex",
+        
+          title: "VNEGNN Bindingsite - 10EX",
+        
+        description: "Isoform 4 of Glutamate receptor ionotropic, NMDA 1 from Homo sapiens. PDB 10EX is titled &quot;SK5A-Matured apo state in complex with GluN1-GluN2B, full refinement.&quot;",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/vnegnn-bindingsite-10ex/";
+          
+        },
+      },{id: "post-vnegnn-bindingsite-10ev",
         
           title: "VNEGNN Bindingsite - 10EV",
         
